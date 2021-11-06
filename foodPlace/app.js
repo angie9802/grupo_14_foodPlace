b607a688453dacbf66304048bcfc7bf9089bb5ee
Juan Andres Valencia <juanandres2018marzo@gmail.com>
2021-11-05 21:19:08 -0500

    work with the teammates on ejs files, mvc project structure

--- foodPlace/app.js
+++ foodPlace/app.js
@@ -1,29 +1,21 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const path = require("path");
+const app = express();
+const mainRouter = require("./src/routes/mainRoutes");
+const port = process.env.PORT || 3000;
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/index.html"))
-})
-app.get('/menu', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/menu.html"))
-})
-app.get('/detail-product', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/detail-product.html"))
-})
-app.get('/shopping-cart', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/shopping-cart.html"))
-})
-app.get('/login', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/login.html"))
-})
-app.get('/register', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/register.html"))
-})
+app.set("view engine", "ejs");
 
-app.use(express.static(path.resolve(__dirname,'./public')))
+//Routes
+app.use("/", mainRouter);
+app.use("/menu", mainRouter);
+app.use("/login", mainRouter);
+app.use("/register", mainRouter);
+app.use("/detail-product", mainRouter);
+app.use("/shopping-cart", mainRouter);
 
-app.listen(port, ()=>
-    console.log("Running server in port "+port)
-)
+
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.listen(port, () => console.log("Running server in port " + port));
