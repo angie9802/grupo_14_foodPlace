b2d3378e7daa800e639cc575120a3c3d75e393ac
Angie Cantor <avbarajasc@unal.edu.co>
2021-12-21 20:13:35 -0500

    little detail

--- foodPlace/src/routes/main.js
+++ foodPlace/src/routes/main.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const mainController = require('../controllers/main')
+const authMiddleware = require('../middlewares/authMiddleware')
 
 router.get('/', mainController.index)
-router.get('/shopping-cart', mainController.shoppingCart)
+
+router.get('/shopping-cart', authMiddleware, mainController.shoppingCart)
 router.get('/create-product', mainController.createProduct)
 
 module.exports = router
