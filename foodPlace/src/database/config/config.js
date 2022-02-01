23579390384bd7752f35c4f24ec217fa2d849a44
Angie Cantor <avbarajasc@unal.edu.co>
2022-02-01 14:33:24 -0500

    commit

--- foodPlace/src/database/config/config.js
+++ foodPlace/src/database/config/config.js
@@ -1,24 +1,26 @@
+const env = process.env
+
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": "password",
-    "database": "foodplace",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": 3307
+  development: {
+    username: "root",
+    password: "password",
+    database: "foodplace",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    port: 3306
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: env.username,
+    password: env.password,
+    database: env.database,
+    host: env.host,
+    dialect: "mysql"
   }
 }
